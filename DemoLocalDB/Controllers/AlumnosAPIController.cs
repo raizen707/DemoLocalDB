7116ffa46aa09c8c0acad00c1d1096747918e85d
Raizen707 <Raizen707@RAIZEN-ROG>
2019-02-15 20:40:37 -0600

    Add project files.

---
+++ DemoLocalDB/Controllers/AlumnosAPIController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.OData;
+using System.Web.Http.OData.Routing;
+using DemoLocalDB.Models;
+
+namespace DemoLocalDB.Controllers
+{
+    /*
+    The WebApiConfig class may require additional changes to add a route for this controller. Merge these statements into the Register method of the WebApiConfig class as applicable. Note that OData URLs are case sensitive.
+
+    using System.Web.Http.OData.Builder;
+    using System.Web.Http.OData.Extensions;
+    using DemoLocalDB.Models;
+    ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+    builder.EntitySet<Alumno>("AlumnosAPI");
+    config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
+    */
+    public class AlumnosAPIController : ODataController
+    {
+        private UniversidadEntities db = new UniversidadEntities();
+
+        // GET: odata/AlumnosAPI
+        [EnableQuery]
+        public IQueryable<Alumno> GetAlumnosAPI()
+        {
+            return db.Alumnoes;
+        }
+
+        // GET: odata/AlumnosAPI(5)
+        [EnableQuery]
+        public SingleResult<Alumno> GetAlumno([FromODataUri] int key)
+        {
+            return SingleResult.Create(db.Alumnoes.Where(alumno => alumno.Id == key));
+        }
+
+        // PUT: odata/AlumnosAPI(5)
+        public IHttpActionResult Put([FromODataUri] int key, Delta<Alumno> patch)
+        {
+            Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Alumno alumno = db.Alumnoes.Find(key);
+            if (alumno == null)
+            {
+                return NotFound();
+            }
+
+            patch.Put(alumno);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AlumnoExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(alumno);
+        }
+
+        // POST: odata/AlumnosAPI
+        public IHttpActionResult Post(Alumno alumno)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Alumnoes.Add(alumno);
+            db.SaveChanges();
+
+            return Created(alumno);
+        }
+
+        // PATCH: odata/AlumnosAPI(5)
+        [AcceptVerbs("PATCH", "MERGE")]
+        public IHttpActionResult Patch([FromODataUri] int key, Delta<Alumno> patch)
+        {
+            Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Alumno alumno = db.Alumnoes.Find(key);
+            if (alumno == null)
+            {
+                return NotFound();
+            }
+
+            patch.Patch(alumno);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AlumnoExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(alumno);
+        }
+
+        // DELETE: odata/AlumnosAPI(5)
+        public IHttpActionResult Delete([FromODataUri] int key)
+        {
+            Alumno alumno = db.Alumnoes.Find(key);
+            if (alumno == null)
+            {
+                return NotFound();
+            }
+
+            db.Alumnoes.Remove(alumno);
+            db.SaveChanges();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AlumnoExists(int key)
+        {
+            return db.Alumnoes.Count(e => e.Id == key) > 0;
+        }
+    }
+}
---
+++ DemoLocalDB/Controllers/AlumnosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DemoLocalDB.Models;
+
+namespace DemoLocalDB.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        private UniversidadEntities db = new UniversidadEntities();
+
+        // GET: Alumnos
+        public ActionResult Index()
+        {
+            return View(db.Alumnoes.ToList());
+        }
+
+        // GET: Alumnos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Alumno alumno = db.Alumnoes.Find(id);
+            if (alumno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alumno);
+        }
+
+        // GET: Alumnos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Alumnos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,nombre,carne")] Alumno alumno)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Alumnoes.Add(alumno);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(alumno);
+        }
+
+        // GET: Alumnos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Alumno alumno = db.Alumnoes.Find(id);
+            if (alumno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alumno);
+        }
+
+        // POST: Alumnos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,nombre,carne")] Alumno alumno)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(alumno).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(alumno);
+        }
+
+        // GET: Alumnos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Alumno alumno = db.Alumnoes.Find(id);
+            if (alumno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alumno);
+        }
+
+        // POST: Alumnos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Alumno alumno = db.Alumnoes.Find(id);
+            db.Alumnoes.Remove(alumno);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
